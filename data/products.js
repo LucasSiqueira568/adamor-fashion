561dacc2bcf616ff60fd021bbec0532567b95f27
LucasSiqueira568 <siqueiralucas-pro@outlook.com>
2021-10-11 22:48:41 -0300

    Base de dados fake para exibição dos produtos

---
+++ data/products.js
@@ -0,0 +1,87 @@
+const sapatos = [
+    {
+        id: 1,
+        name: 'Nike Air Max 90',
+        price: 119.90,
+        description: 'Um dos Tenis mais lindos dos ultimos anos, agora bem pertinho de você',
+        img: require("../src/assets/tenis-banner.png")
+    },
+    {
+        id: 2,
+        name: 'Nike Air Max 90',
+        price: 119.90,
+        description: 'Um dos Tenis mais lindos dos ultimos anos, agora bem pertinho de você',
+        img: require("../src/assets/tenis-banner-2.png")
+    },
+    {
+        id: 3,
+        name: 'Nike Air Max 90',
+        price: 119.90,
+        description: 'Um dos Tenis mais lindos dos ultimos anos, agora bem pertinho de você',
+        img: require("../src/assets/tenis-banner-3.png")
+    },
+    {
+        id: 4,
+        name: 'Nike Air Max 90',
+        price: 119.90,
+        description: 'Um dos Tenis mais lindos dos ultimos anos, agora bem pertinho de você',
+        img: require("../src/assets/tenis-banner-4.png")
+    },
+    {
+        id: 5,
+        name: 'Nike Air Max 90',
+        price: 119.90,
+        description: 'Um dos Tenis mais lindos dos ultimos anos, agora bem pertinho de você',
+        img: require("../src/assets/tenis-banner-5.png")
+    },
+    {
+        id: 6,
+        name: 'Nike Air Max 90',
+        price: 119.90,
+        description: 'Um dos Tenis mais lindos dos ultimos anos, agora bem pertinho de você',
+        img: require("../src/assets/tenis-banner-6.png")
+    },
+]
+
+const camisas = [
+    {
+        name: 'Camisa Nike',
+        price: 119.90,
+        description: 'Um dos Tenis mais lindos dos ultimos anos, agora bem pertinho de você',
+        img: require("../src/assets/camisa-1.png")
+    },
+    {
+        name: 'Camisa Nike',
+        price: 119.90,
+        description: 'Um dos Tenis mais lindos dos ultimos anos, agora bem pertinho de você',
+        img: require("../src/assets/camisa-2.png")
+    },
+    {
+        name: 'Camisa Nike',
+        price: 119.90,
+        description: 'Um dos Tenis mais lindos dos ultimos anos, agora bem pertinho de você',
+        img: require("../src/assets/camisa-3.png")
+    },
+    {
+        name: 'Camisa Nike',
+        price: 119.90,
+        description: 'Um dos Tenis mais lindos dos ultimos anos, agora bem pertinho de você',
+        img: require("../src/assets/camisa-4.png")
+    },
+    {
+        name: 'Camisa Nike',
+        price: 119.90,
+        description: 'Um dos Tenis mais lindos dos ultimos anos, agora bem pertinho de você',
+        img: require("../src/assets/camisa-5.png")
+    },
+    {
+        name: 'Camisa Nike',
+        price: 119.90,
+        description: 'Um dos Tenis mais lindos dos ultimos anos, agora bem pertinho de você',
+        img: require("../src/assets/camisa-6.png")
+    },
+]
+
+const products = sapatos 
+
+export default products;
